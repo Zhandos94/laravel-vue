937771a481e1d49d0020d0e48c34b532b58fc545
zh.zhumagaly <zh.zhumagaly@gmail.com>
2020-02-19 19:49:25 +0600

    frontend

---
+++ frontend/pages/users/_id/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>User {{ $route.params.id }}</h1>
+    </div>
+  </div>
+</template>
---
+++ frontend/pages/users/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+
+    <ul class="nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+
+    <div>
+      <h1>users</h1>
+      <input type="text" v-model="id">
+      <button @click="loadUser">Load user</button>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+      data() {
+         return {
+           id: ''
+         }
+      },
+      methods: {
+        loadUser() {
+          this.$router.push(`/users/${this.id}`)
+        }
+      }
+    }
+</script>
